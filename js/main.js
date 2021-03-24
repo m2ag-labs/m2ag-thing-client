72a8ff10452fa3f615fadfb5b6f2c0a348584956
marc <mbgraham2000@gmail.com>
2021-03-24 11:22:03 -0700

    tweak

--- js/main.js
+++ js/main.js
@@ -234,7 +234,16 @@ function mainActionHandler() {
                 if (src.includes('index.html')) {
                     document.getElementById('thing_ui_url').innerText = "select a thing and try again"
                 } else {
+                    let uri = window.location.protocol === 'https:' ? 'wss://' : 'ws://'
+                    uri += window.location.hostname + ':8888'
+                    const split = src.substr(src.indexOf('index=')).split('&')
+                    let path = '/'
+                    let auth = '?'
+                    path += split[0].split('=')[1]
+                    auth += split[split.length - 1]
                     document.getElementById('thing_ui_url').innerText = src
+                    document.getElementById('thing_ws_url').innerText = `${uri}${path}${auth}`
+
                 }
                 $("#thing_ui_modal").modal("show")
             } else {
