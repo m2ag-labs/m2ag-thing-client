7e6852ebe17a7f7550041d511782dd2325d464fb
marc <mbgraham2000@gmail.com>
2021-05-05 17:05:04 -0700

    wip -- re-work tabs

--- js/tree.js
+++ js/tree.js
@@ -1,10 +1,11 @@
 const device_tree_area = $('#device_tree_area');
+const all_pills = document.getElementsByClassName('all_pills')
 let current_node = ''
 let editors = {}
 
 
 
-const editorInit = () =>{
+const editorsInit = () =>{
     editors['service'] = ace.edit("service_editor")
     editors['thing'] = ace.edit("thing_editor")
     editors['helper'] = ace.edit("helper_editor")
@@ -15,21 +16,58 @@ const editorInit = () =>{
 
 }
 
-function treeInit() {
-    editorInit()
-    device_tree_area.on("changed.jstree", function (e, data) {
-        current_node = ''
-        document.getElementById('ui_frame').src = ''
-        if (data.node !== undefined && data.node.data !== null) {
-            fetch(`${api_url}/${data.node.data}`, getDataOptions)
-                .then(response => response.json())
-                .then(result => {
-                    current_node = result.data
-                    setDetail(data.node, result)
-                })
-                .catch(error => console.log('error', error))
+const editorsClear = () => {
+    for(const i in editors){
+        editors[i].session.setValue('no data')
+        editors[i].session.setMode("ace/mode/text")
+    }
+}
+
+const tabsOff = () => {
+    for(const i of all_pills){
+        i.style.display = 'none'
+    }
+}
+
+const handleMoveNode = (data) => {
+    let update = {"enabled": []}
+    for (let i of data.instance.get_json()[0].children) {
+        if (i.text === 'enabled') {
+            for (let j of i.children) {
+                update.enabled.push(j.text)
+            }
+            break
         }
-    });
+    }
+
+    putDataOptions.body = JSON.stringify(update)
+    fetch(`${api_url}/config/enabled`, putDataOptions)
+        .then(response => response.json())
+        .then(result => {
+            create_device_tree(result)
+        })
+        .catch(error => console.log('error', error))
+
+}
+
+const handleChangeTree = (data) => {
+    current_node = ''
+    document.getElementById('ui_frame').src = ''
+    if (data.node !== undefined && data.node.data !== null) {
+        fetch(`${api_url}/${data.node.data}`, getDataOptions)
+            .then(response => response.json())
+            .then(result => {
+                current_node = result.data
+                setDetail(data.node, result)
+            })
+            .catch(error => console.log('error', error))
+    }
+}
+
+
+const treeInit = () => {
+    editorsInit()
+    tabsOff()
     device_tree_area.jstree({
         "types": {
             "default": {
@@ -39,7 +77,7 @@ function treeInit() {
                 "icon": "../../css/images/speedometer2.svg"
             },
             "service": {
-                "icon": "../../css/images/wrench.svg"
+                "icon": "../../css/images/led-green.png"
             }
         },
         "dnd": {
@@ -48,9 +86,6 @@ function treeInit() {
         "plugins": ["dnd", "types", "wholerow"],
         "core": {
             'check_callback': function (operation, node, node_parent, node_position, more) {
-                // operation can be 'create_node', 'rename_node', 'delete_node', 'move_node' or 'copy_node'
-                // in case of 'rename_node' node_position is filled with the new node name
-
                 if (operation === "move_node") {
                     return node.type === "thing" && node_parent.original.type === "component"; //only allow dropping inside nodes of type 'Parent'
                 }
@@ -58,36 +93,25 @@ function treeInit() {
             }
         }
     });
-    device_tree_area.bind("move_node.jstree", function (e, data) {
+    device_tree_area.on("changed.jstree", function (e, data) {
+        handleChangeTree(data)
+    });
+    device_tree_area.bind("move_node.jstree", function (e,data) {
+        handleMoveNode(data)
+    });
         // get the current enabled list, write to server, update tree from response
         // set current state and set tree back to the same.
-        let update = {"enabled": []}
-        for (let i of data.instance.get_json()[0].children) {
-            if (i.text === 'enabled') {
-                for (let j of i.children) {
-                    update.enabled.push(j.text)
-                }
-                break
-            }
-        }
 
-        putDataOptions.body = JSON.stringify(update)
-        fetch(`${api_url}/config/enabled`, putDataOptions)
-            .then(response => response.json())
-            .then(result => {
-                create_device_tree(result)
-            })
-            .catch(error => console.log('error', error))
-    });
 }
 
-
-function setDetail(node) {
-
+const setDetail = (node) => {
+    editorsClear()
     if ('index' in node.original) {
+        tabsOff()
         switch (node.original.index) {
             case 'm2ag-thing-tag':
                 editors.service.session.setMode("ace/mode/json")
+                document.getElementsByClassName('thing_pill').item(0).style.display = 'block'
                 editors.thing.session.setMode("ace/mode/json")
                 editors.helper.session.setMode("ace/mode/python")
                 editors.service.session.setValue(JSON.stringify(current_node.service, null, 2))
@@ -95,17 +119,20 @@ function setDetail(node) {
                 editors.thing.session.setValue(JSON.stringify(current_node.thing, null, 2))
                 editors.thing.setReadOnly(true);
                 if ('ui' in node.original) {
+                    document.getElementsByClassName('ui_pill').item(0).style.display = 'block'
                     document.getElementById('ui_frame').src =
                         `${window.location.origin}/ui/raspiui.html?index=${node.original.ui}&socket=true&jwt=${jwt_token}`
                 }
                 if ('helper' in current_node && current_node.helper !== 'none'){
+                    document.getElementsByClassName('helper_pill').item(0).style.display = 'block'
                     editors.helper.session.setValue(current_node.helper)
                     editors.helper.setReadOnly(true);
                 }
                 break
             case 'm2ag-server-tag':
-                editors.service.setMode("ace/mode/json")
-                editors.service.session.setValue(JSON.stringify(current_node.service, null, 2))
+                editors.service.session.setMode("ace/mode/json")
+                editors.service.session.setValue(JSON.stringify(current_node, null, 2))
+                document.getElementsByClassName('service_pill').item(0).style.display = 'block'
                 editors.service.setReadOnly(true)
                 //TODO: - test this - can I get to node red ui from here?
                 if ('ui' in node.original) {
@@ -115,6 +142,7 @@ function setDetail(node) {
             case 'm2ag-service-tag':
                 editors.service.session.setMode("ace/mode/text")
                 editors.service.session.setValue(current_node)
+                document.getElementsByClassName('service_pill').item(0).style.display = 'block'
                 editors.service.setReadOnly(true)
                 break
             default:
@@ -126,7 +154,7 @@ function setDetail(node) {
 
 
 // Tree functions
-function disableTree() {
+const disableTree = () => {
     // disable visible nodes
     $('#device_tree_area li.jstree-node').each(function () {
         $('#device_tree_area').jstree("disable_node", this.id)
@@ -138,7 +166,7 @@ function disableTree() {
 
 }
 
-function enableTree() {
+const enableTree = () => {
     // enable again visible nodes
     $('#device_tree_area li.jstree-node').each(function () {
         $('#device_tree_area').jstree("enable_node", this.id)
@@ -147,7 +175,7 @@ function enableTree() {
     $('#device_tree_area i.jstree-ocl').off('click.block');
 }
 
-function create_device_tree(response) {
+const create_device_tree = (response) => {
     let data = response.data
     let root = {
         "icon": "../../css/images/cpu.svg",
@@ -170,6 +198,7 @@ function create_device_tree(response) {
     for (let i in data.available) { // jshint ignore:line
         if (data.available.hasOwnProperty(i) && data.enabled.indexOf(data.available[i]) < 0) {
             root.children[root.children.length - 1].children.push({
+                "icon": "../../css/images/file-text.svg",
                 "type": "thing",
                 "text": data.available[i],
                 "index": "m2ag-thing-tag",
@@ -221,6 +250,7 @@ function create_device_tree(response) {
     device_tree_area.jstree(true).settings.core.data = [root];
     device_tree_area.jstree(true).refresh();
 }
+
 
 
 
