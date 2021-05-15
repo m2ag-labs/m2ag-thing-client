79e3907eb04b9918f1a82363de066ab18ec53d78
marc <mbgraham2000@gmail.com>
2021-05-14 17:26:38 -0700

    wip move to more iframe use

--- js/tree.js
+++ js/tree.js
@@ -1,37 +1,14 @@
 const device_tree_area = $('#device_tree_area');
 const all_pills = document.getElementsByClassName('all_pills')
 let current_node = ''
+let current_data = ''
 let current_view = 'none'
-let editors = {}
 
-
-const editorsInit = () => {
-    editors.service = ace.edit("service_editor")  // jshint ignore:line
-    editors.thing = ace.edit("thing_editor") // jshint ignore:line
-    editors.helper = ace.edit("helper_editor") // jshint ignore:line
-    for (const i in editors) {
-        if (editors.hasOwnProperty(i)) {
-            editors[i].setTheme("ace/theme/chrome")
-            editors[i].setOptions({fontSize: "12pt"});
-        }
-    }
-
-}
-
-const editorsClear = () => {
-    for (const i in editors) {
-        if (editors.hasOwnProperty(i)) {
-            editors[i].session.setValue('no data')
-            editors[i].session.setMode("ace/mode/text")
-        }
-    }
-}
-const service_tab = new bootstrap.Tab(document.querySelector('#service-tab'))
+const thing_tab = new bootstrap.Tab(document.querySelector('#thing-tab'))
 const tabsOff = (view) => {
     if(view !== current_view) {
         current_view = view
-        service_tab.show()
-        document.getElementById('service-button-bar').style.display = 'none'
+        thing_tab.show()
     }
      for (const i of all_pills) {
             i.style.display = 'none'
@@ -53,7 +30,6 @@ const handleMoveNode = (data) => {
             break
         }
     }
-
     putDataOptions.body = JSON.stringify(update) // jshint ignore:line
     fetch(`${api_url}/config/enabled`, putDataOptions)// jshint ignore:line
         .then(response => response.json())
@@ -61,44 +37,34 @@ const handleMoveNode = (data) => {
             create_device_tree(result)
         })
         .catch(error => console.log('error', error))
-
 }
 
 
 const setDetail = (node) => {
-    editorsClear()
     if ('index' in node.original) {
         tabsOff(node.original.index)
         switch (node.original.index) {
             case 'm2ag-thing-tag':
                 if ('ui' in node.original) {
-                    //document.getElementsByClassName('ui_pill').item(0).style.display = 'block'
-                   // document.getElementById('ui_frame').src =
-                    //    `${window.location.origin}/ui/raspiui.html?index=${node.original.ui}&socket=true&jwt=${jwt_token}` // jshint ignore:line
+                    document.getElementsByClassName('ui_pill').item(0).style.display = 'block'
+                    document.getElementById('ui_frame').src =
+                       `${window.location.origin}/ui/raspiui.html?index=${node.original.ui}&socket=true&jwt=${jwt_token}` // jshint ignore:line
                 }
-                if ('helper' in current_node && current_node.helper !== 'none') {
-                //    document.getElementsByClassName('helper_pill').item(0).style.display = 'block'
-                //    editors.helper.session.setValue(current_node.helper)
-                //    editors.helper.setReadOnly(true);
+                if ('helper' in node.original && node.original.helper !== false) {
+                    document.getElementsByClassName('helper_pill').item(0).style.display = 'block'
+                    document.getElementById('helper_frame').src =
+                       `${window.location.origin}/ui/editor.html?path=${node.original.helper}&type=python&auth=${auth_hash}`
+
                 }
                 break
             case 'm2ag-server-tag':
-                editors.service.session.setMode("ace/mode/json")
-                editors.service.session.setValue(JSON.stringify(current_node, null, 2))
                 document.getElementsByClassName('service_pill').item(0).style.display = 'block'
-                editors.service.setReadOnly(true)
-                //TODO: - test this - can I get to node red ui from here?
                 if ('ui' in node.original) {
                     document.getElementById('ui_frame').src = node.original.ui
                 }
                 break
             case 'm2ag-service-tag':
-                editors.service.session.setMode("ace/mode/text")
-                editors.service.session.setValue(current_node)
-                setServiceButtons()
-                document.getElementById('service-button-bar').style.display = 'block'
-                document.getElementById('service-button-label').innerText = node.original.text
-                editors.service.setReadOnly(true)
+
                 break
             default:
                 break
@@ -107,59 +73,7 @@ const setDetail = (node) => {
 
 }
 
-const setServiceButtons = () =>{
-    const isRunning = current_node.split("\n")[2].trim().indexOf(`(running)`) > -1
-    const isEnabled = current_node.split("\n")[1].trim().indexOf(`service; enabled;`) > -1
-    if(isRunning){
-        document.getElementById("service-start-button").innerText = 'stop'
-        document.getElementById("service-start-button").classList.remove('btn-success')
-        document.getElementById("service-start-button").classList.add('btn-danger')
-    } else {
-        document.getElementById("service-start-button").innerText = 'start'
-        document.getElementById("service-start-button").classList.remove('btn-danger')
-        document.getElementById("service-start-button").classList.add('btn-success')
-    }
-    if(isEnabled){
-        document.getElementById("service-enable-button").innerText = 'disable'
-        document.getElementById("service-enable-button").classList.remove('btn-success')
-        document.getElementById("service-enable-button").classList.add('btn-danger')
-    } else {
-        document.getElementById("service-enable-button").innerText = 'enable'
-        document.getElementById("service-enable-button").classList.remove('btn-danger')
-        document.getElementById("service-enable-button").classList.add('btn-success')
-    }
 
-}
-
-
-const serviceButtonClick = (action) => {
-    console.log(action)
-    let endpoint = document.getElementById('service-button-label').innerText + '/'
-    switch(action){
-        case 'enable':
-            if(document.getElementById("service-enable-button").innerText === 'enable'){
-                endpoint += 'enable'
-            } else {
-                endpoint +=  'disable'
-            }
-            break
-        case 'start':
-            if(document.getElementById("service-start-button").innerText === 'start'){
-                endpoint += 'start'
-            } else {
-                endpoint +=  'stop'
-            }
-            break
-    }
-    fetch(`${api_url}/${endpoint}`, getDataOptions)
-                .then(response => response.json())
-                .then(result => {
-                    editors.service.session.setValue(result.data)
-                    current_node = result.data
-                    setServiceButtons()
-                })
-                .catch(error => console.log('error', error))
-}
 
 const handleChangeTree = (data) => {
     current_node = ''
@@ -198,10 +112,10 @@ const enableTree = () => { // jshint ignore:line
 }
 
 const create_device_tree = (response) => { // jshint ignore:line
-    let data = response.data
+    let current_data = response.data
     let root = {
         "icon": "../../css/images/cpu.svg",
-        "text": data.server.id,
+        "text": current_data.server.id,
         "data": `config/server`,
         "index": 'm2ag-server-tag',
         "state": {"opened": true},
@@ -217,14 +131,19 @@ const create_device_tree = (response) => { // jshint ignore:line
         "children": []
     });
 
-    for (let i in data.available) { // jshint ignore:line
-        if (data.available.hasOwnProperty(i) && data.enabled.indexOf(data.available[i]) < 0) {
+    for (let i in current_data.available) { // jshint ignore:line
+        if (current_data.available.hasOwnProperty(i) && current_data.enabled.indexOf(current_data.available[i]) < 0) {
+            let helper = false
+            if(current_data.helpers.indexOf(current_data.available[i]) !== -1) {
+                helper = `config/helpers/${current_data.available[i]}`
+            }
             root.children[root.children.length - 1].children.push({
                 "icon": "../../css/images/file-text.svg",
                 "type": "thing",
-                "text": data.available[i],
+                "text": current_data.available[i],
                 "index": "m2ag-thing-tag",
-                "data": `config/things/${data.available[i]}`
+                "data": `config/things/${current_data.available[i]}`,
+                "helper": helper
             });
         }
     }
@@ -238,14 +157,19 @@ const create_device_tree = (response) => { // jshint ignore:line
         "children": []
     });
 
-    for (let i in data.enabled) { // jshint ignore:line
-        if (data.enabled.hasOwnProperty(i)) {
+    for (let i in current_data.enabled) { // jshint ignore:line
+        if (current_data.enabled.hasOwnProperty(i)) {
+            let helper = false
+            if(current_data.helpers.indexOf(current_data.enabled[i]) !== -1) {
+                helper = `config/helpers/${current_data.enabled[i]}`
+            }
             root.children[root.children.length - 1].children.push({
                 "type": "thing",
-                "text": data.enabled[i],
+                "text": current_data.enabled[i],
                 "index": "m2ag-thing-tag",
                 "ui": i,
-                "data": `config/things/${data.enabled[i]}`
+                "data": `config/things/${current_data.enabled[i]}`,
+                "helper": helper
             });
         }
     }
@@ -256,12 +180,12 @@ const create_device_tree = (response) => { // jshint ignore:line
         "state": {"opened": true},
         "children": []
     });
-    for (let i in data.services) { // jshint ignore:line
-        if (data.services.hasOwnProperty(i)) {
+    for (let i in current_data.services) { // jshint ignore:line
+        if (current_data.services.hasOwnProperty(i)) {
             root.children[root.children.length - 1].children.push({
                 "type": "service",
-                "text": data.services[i],
-                "data": `${data.services[i]}/status`,
+                "text": current_data.services[i],
+                "data": `${current_data.services[i]}/status`,
                 "index": "m2ag-service-tag"
             });
         }
@@ -272,7 +196,6 @@ const create_device_tree = (response) => { // jshint ignore:line
 
 
 const treeInit = () => { // jshint ignore:line
-    editorsInit()
     tabsOff()
     device_tree_area.jstree({
         "types": {
@@ -305,7 +228,5 @@ const treeInit = () => { // jshint ignore:line
     device_tree_area.bind("move_node.jstree", function (e,data) { // jshint ignore:line
         handleMoveNode(data)
     });
-    // get the current enabled list, write to server, update tree from response
-    // set current state and set tree back to the same.
 
 }
