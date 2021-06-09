563c7e93ae36cd6423bbd710a1b45f5bfe04d91a
marc <mbgraham2000@gmail.com>
2021-06-09 10:01:05 -0700

    thingapi page - tweaks -- move from firebase to google cloud storage. The getter files will be static json

--- js/main.js
+++ js/main.js
@@ -11,24 +11,6 @@ const jwtModal = (mode) => {
  }
 
 
-const mainActionHandler = (id) => {
-    const src = document.getElementById('ui_frame').src
-                if (src.includes('index.html')) {
-                    document.getElementById('thing_ui_url').innerText = "select a thing and try again"
-                } else {
-                    let uri = window.location.protocol === 'https:' ? 'wss://' : 'ws://'
-                    uri += window.location.hostname + ':8888'
-                    const split = src.substr(src.indexOf('index=')).split('&')
-                    let path = '/'
-                    let auth = '?'
-                    path += split[0].split('=')[1]
-                    auth += split[split.length - 1]
-                    document.getElementById('thing_ui_url').innerText = src
-                    document.getElementById('thing_ws_url').innerText = `${uri}${path}${auth}`
-
-                }
-        }
-
 
 
 
